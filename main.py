5978e3e4c5be5ebc568aeb9861c13b79eff4072e
Albie <albiegamer6544@gmail.com>
2021-11-30 16:36:26 -0500

    Some Polish

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 # IMPORTS AND GLOBALS #
 #######################
 
+
 import os
+import re
 import time
+import urllib
 import discord
 import validators
 
@@ -26,17 +29,20 @@ async def actionplay(ctx, args):
             url = str(args[0])
             if validators.url(url):
                 await ctx.send("Retrieving video file...")
-                file = getvideo(url)
-                await ctx.send("Playing audio file...")
-                if not isconnected(ctx):
-                    await ctx.author.voice.channel.connect()
-                if ctx.voice_client.is_playing():
-                    ctx.voice_client.pause()
-                ctx.voice_client.play(discord.FFmpegPCMAudio(file[0]))
+                file = getvideo(ctx, url)
+                if file:
+                    await ctx.send("Playing audio file...")
+                    if not isconnected(ctx):
+                        await ctx.author.voice.channel.connect()
+                    if ctx.voice_client.is_playing():
+                        ctx.voice_client.pause()
+                    ctx.voice_client.play(discord.FFmpegPCMAudio(file[0]))
             else:
                 await ctx.send("Searching query...")
-                # Get youtube search result as a link
-                # actionplay(ctx, [search])
+                html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + "+".join(args))
+                videos = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+                await ctx.send("Found video: https://www.youtube.com/watch?v=" + videos[0])
+                await actionplay(ctx, "https://www.youtube.com/watch?v=" + videos[0])
         else:
             await ctx.send("You must enter a valid URL or search query...")
     else:
@@ -70,10 +76,13 @@ async def actionleave(ctx):
         await ctx.send("The bot is not currently connected...")
 
 
-def getvideo(url):
+def getvideo(ctx, url):
     millis = time.time() * 1000
-    console = (os.popen("youtube -o media/" + str(millis) + ".mp3 -x --audio-format mp3 " + url).read())
-    return ["media/" + str(millis) + ".mp3", console]
+    try:
+        console = (os.popen("youtube -o media/" + str(millis) + ".mp3 -x --audio-format mp3 " + url).read())
+        return["media/" + str(millis) + ".mp3", console]
+    except:
+        ctx.send("The video could not be downloaded...")
 
 
 def isconnected(ctx):
@@ -82,7 +91,7 @@ def isconnected(ctx):
 
 
 def readconfig():
-    file = open(".confg", "r")
+    file = open(".config", "r")
     lines = file.read().splitlines()
     file.close()
     return lines

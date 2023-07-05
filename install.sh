28bf9232074a5bece57403c88ff53c2a6f5d39c8
Albie <49698766+albie6544@users.noreply.github.com>
2023-07-04 22:58:32 -0400

    Updated Node

--- install.sh
+++ install.sh
@@ -2,14 +2,9 @@ echo "Updating Package Manager..."
 sudo apt update
 
 echo "Installing Node.js/NPM..."
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt install -y nodejs
 sudo apt install -y npm
-
-echo "Updating Node.js"
-sudo npm cache clean -f
-sudo npm install -g n
-sudo n stable
-sudo apt-get install --reinstall nodejs-legacy
 
 echo "Updating Systems..."
 sudo apt upgrade -y

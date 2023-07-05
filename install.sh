c7466baa99bfa9fda069356dbaca48870255c2a8
Albie <49698766+albie6544@users.noreply.github.com>
2023-07-04 22:30:02 -0400

    Ubuntu Node Outdated

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ echo "Installing Node.js/NPM..."
 sudo apt install -y nodejs
 sudo apt install -y npm
 
+echo "Updating Node.js"
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+sudo apt-get install --reinstall nodejs-legacy
+
 echo "Updating Systems..."
 sudo apt upgrade -y
 
